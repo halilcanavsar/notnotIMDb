ee1a26ae57730dbdef39d12c9801f21b7605da5c
Halil Can Avşar <halilcanavsar@Halils-MacBook-Pro.local>
2022-06-13 10:24:24 +0300

    single page change

--- client/src/pages/SingleShowPage.js
+++ client/src/pages/SingleShowPage.js
@@ -1,10 +1,8 @@
-import { useEffect, useContext }  from 'react';
+import { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import ShowsContext from '../contexts/shows/showsContext';
 
-
 const SinglePage = () => {
-
   const { setSingleShow, singleShow, loading } = useContext(ShowsContext);
   const { id } = useParams();
 
@@ -19,58 +17,99 @@ const SinglePage = () => {
       text = text.toString();
     }
     return text.replace(/<[^>]*>/g, '');
-  }
-  console.log('singleshow', singleShow);
+  };
+
+
+
   return (
     <>
-      {singleShow &&
-      <div className="single-show">
-        <div className="single-show-image">
-          <img src={singleShow.image ? singleShow.image.medium : 'https://via.placeholder.com/210x295'} alt={singleShow.name} />
-        </div>
-        <div className="single-show-info">
-          <h3 className="single-show-name">{singleShow.name}</h3>
-          {singleShow.genres && singleShow.genres.map(genres => <p key={genres} className="single-show-genre">{genres}</p>)}
-
-          <p>
-          <strong>Status: </strong> { singleShow.status && singleShow.status }
-          </p>
-
-          <p>
-          <strong>Rating: </strong> { singleShow.rating ? singleShow.rating.average : '-' }
-          </p>
-
-          <p>
-            <strong>Official Site: </strong> { singleShow.officialSite ? (<a href={singleShow.officialSite} target="_blank" rel="noreferrer" >{singleShow.officialSite}</a>) : '-' }
-          </p>
-
-          <p>
-            <strong>Summary: </strong> { singleShow.summary && removeTags(singleShow.summary) }
-          </p>
-          <div single-show-episode>
-          { singleShow._embedded.episodes && singleShow._embedded.episodes.map(episode => (
-            <div key={episode.id} className="single-show-episode">
-              <div className="single-show-episode-image">
-                <img src={ episode.image ? episode.image.medium  : 'https://via.placeholder.com/210x295'} alt={episode.name} />
-              </div>
-              <div className="single-show-episode-title">{episode.name}:</div>
-              <p className="single-show-episode-summary">{removeTags(episode.summary)}</p>
-              <div className="single-show-episode-rating"> {episode.rating ? episode.rating.average : '-'} </div>
-              <p className="single-show-episode-season-episode">Season: { episode.season } Episode: {episode.number}</p>
-            </div>
-          ))}
+      {singleShow && (
+        <div className="single-show">
+          <div className="single-show-image">
+            <img
+              src={
+                singleShow.image
+                  ? singleShow.image.medium
+                  : 'https://via.placeholder.com/210x295'
+              }
+              alt={singleShow.name}
+            />
           </div>
+          <div className="single-show-info">
+            <h3 className="single-show-name">{singleShow.name}</h3>
+            {singleShow.genres &&
+              singleShow.genres.map((genres) => (
+                <p key={genres} className="single-show-genre">
+                  {genres}
+                </p>
+              ))}
+
+            <p>
+              <strong>Status: </strong> {singleShow.status && singleShow.status}
+            </p>
+
+            <p>
+              <strong>Rating: </strong>{' '}
+              {singleShow.rating ? singleShow.rating.average : '-'}
+            </p>
+
+            <p>
+              <strong>Official Site: </strong>{' '}
+              {singleShow.officialSite ? (
+                <a
+                  href={singleShow.officialSite}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {singleShow.officialSite}
+                </a>
+              ) : (
+                '-'
+              )}
+            </p>
+
+            <p>
+              <strong>Summary: </strong>{' '}
+              {singleShow.summary && removeTags(singleShow.summary)}
+            </p>
+            <div single-show-episode>
+              {singleShow._embedded.episodes &&
+                singleShow._embedded.episodes.map((episode) => (
+                  <div key={episode.id} className="single-show-episode">
+                    <div className="single-show-episode-image">
+                      <img
+                        src={
+                          episode.image
+                            ? episode.image.medium
+                            : 'https://via.placeholder.com/210x295'
+                        }
+                        alt={episode.name}
+                      />
+                    </div>
+                    <div className="single-show-episode-info">
+                      <div className="single-show-episode-title">
+                        {episode.name}: {removeTags(episode.summary)}
+                      </div>
 
 
 
+                      <div className="single-show-episode-rating">
 
+                        Rating: {episode.rating ? episode.rating.average : '-'}
+                      </div>
 
+                      <p className="single-show-episode-season-episode">
+                        Season: {episode.season} Episode: {episode.number}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
         </div>
-      </div>
-        }
-
+      )}
     </>
-   );
-}
+  );
+};
 
 export default SinglePage;
